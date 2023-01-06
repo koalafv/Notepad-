78b5ad981b02ed441220522c54370ac280233a29
1kls <lukaszkramorz112@gmail.com>
2023-01-06 21:35:43 +0100

    basics functions

--- Notatnik/MainWindow.xaml.cs
+++ Notatnik/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,6 +24,51 @@ namespace Notatnik
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+
+        }
+
+
+        private void StackPanel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+        private int click = 0;
+
+        private void btnMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            if(this.WindowState== WindowState.Maximized)
+            {
+                click++;
+            }
+            click++;
+            if (click == 1)
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+            else if (click == 2)
+            {
+                this.WindowState = WindowState.Normal;
+                click = 0;
+            }
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
